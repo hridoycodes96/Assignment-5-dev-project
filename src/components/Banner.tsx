2370e88327c15ec191d33dfbb124cdc768fc7559
Hridoypaul999 <hridoy.kumar.paul.web@gmail.com>
2026-09-11 18:19:31 +0600

    navbar add

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,8 +4,8 @@ const Banner = () => {
     return (
         <div className='flex  items-center pt-25 container mx-auto'>
             <div className='w-[50%]'>
-                <h1 className='text-7xl font-bold'>Build your Ideal</h1>
-                <h1 className='text-7xl font-bold'>Development Stack</h1>
+                <h1 className='text-5xl font-bold'>Build your Ideal</h1>
+                <h1 className='text-5xl font-bold'>Development Stack</h1>
                 <p className='mt-6'>Explore frontend,backend,database,and tooling options,compare them side by side,and put together the stack that fits your next project.</p>
 
                 <div className='mt-14'>
